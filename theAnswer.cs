3de85612cf2c920d9a780fa730a0beeea595cbdb
Michael Bäck <majkieboy@gmail.com>
2021-03-28 14:17:33 +0200

    Looking for the answer to Douglas Adams famous question.

---
+++ theAnswer.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TheQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theAnswer = '*'; // the wildcard character for matching 'anything'
+            Console.Write("The answer to life, the universe and everything is: ");
+            Console.WriteLine(theAnswer);
+        }
+    }
+}
